6a4f6d74962bfe969296520bc1178cdda70cb251
Matthuris <175315171+Matthuris@users.noreply.github.com>
2024-10-04 15:38:39 +0100

    Refactor displayOp and clearDisplay funcs

--- script.js
+++ script.js
@@ -16,24 +16,39 @@ operate = function (a, op, b) {
   if (op === "x") return multiply(a, b);
   if (op === "÷") return divide(a, b);
 };
-let displayValue;
+let operation = [];
+let number = null;
 display = document.querySelector(".display");
 inputs = document.querySelector(".inputs");
 clear = document.querySelector(".clear");
+equals = document.querySelector(".equals");
 displayOp = function () {
   inputs.addEventListener("click", function (e) {
-    if (e.target.classList.contains("valid-display")) {
+    if (e.target.classList.contains("number")) {
       display.textContent += e.target.textContent;
-      displayValue = display.textContent;
+      number = display.textContent;
+    }
+    if (e.target.classList.contains("operator")) {
+      operation.push(number);
+      number = null;
+      display.textContent = "";
+      operation.push(e.target.textContent);
+      console.log(operation);
     }
   });
 };
 clearDisplay = function () {
   clear.addEventListener("click", function (e) {
     display.textContent = "";
-    displayValue = display.textContent;
+    number = null;
+    operation = [];
   });
 };
-calculate;
+calculate = function () {
+  equals.addEventListener("click", function () {
+    console.log(operation);
+  });
+};
 displayOp();
 clearDisplay();
+calculate();
